327fbd2f4ceba7752366c2d14df967d9b4050580
Tkst-tomar <92792070+Tkst-tomar@users.noreply.github.com>
2021-12-18 09:49:54 -0800

    Add files via upload

--- app.js
+++ app.js
@@ -84,6 +84,13 @@ document.getElementById("data").innerHTML = tbl
 var count = 0
 var count1 = 0
 var index = 0
+
+function resetTransaction(){
+    document.getElementById("container").innerHTML = `<input type="text" id="name" placeholder="Enter User Name">
+    <button id="btn" onclick="submit()">Submit</button>`
+}
+
+
 function change(vale){
     document.getElementById("container").innerHTML = `<input type="password" id="pass" value = '${vale}' placeholder="Enter Password"><i onclick = 'changeAgain(this.parentElement.firstChild.value)' class="fas fa-eye"></i>
     <button id="btn" onclick = 'check(this.parentElement.firstChild.value)'>Submit</button>`
@@ -101,20 +108,28 @@ function with1(money){
          <button id="btn" onclick = 'with1(this.parentElement.firstChild.value)' >Submit</button>`
        
     }else{
-        document.getElementById("container").innerHTML = `<p>Amount ${money} has been withdrawn from your account.<br>Remaining Balance ${data[index].task.withdrawl(money,index)}</p><button id="btn" onclick="submit()">Click to complete your Transaction</button><br>`
+        document.getElementById("container").innerHTML = `<p>Amount ${money} has been withdrawn from your account.<br>Remaining Balance ${data[index].info.amount}</p><button id="btn" onclick="resetTransaction()">Click to complete your Transaction</button><br>`
+        count = 0
+        count1 = 0
     }
 }
 
 function check(x){
     console.log(index)
-        if(data[index].task.getpassword(x,index) == 1 && count1 < 3){
+    if(count1 < 3){
+        if(data[index].task.getpassword(x,index) == 1){
          document.getElementById("container").innerHTML = `<input type="text" id="mon" placeholder="Enter Amount To Be Withdrawn">
          <button id="btn" onclick = 'with1(this.parentElement.firstChild.value)' >Submit</button>`
         }else{
          document.getElementById("container").innerHTML = `<input type="password" id="pass" placeholder="Enter Password"><i onclick = 'changeAgain(this.parentElement.firstChild.value)' class="fas fa-eye"></i><p>You Have Entered Wrong Password, ${3-count1} attempt left</p>
          <button id="btn" onclick = 'check(this.parentElement.firstChild.value)'>Submit</button>`
-         count++
         }
+        count1++
+        
+    }else{
+        document.getElementById("container").innerHTML =`Your Account has been blocked <br>Please try again in 24 hours <button id = 'btn' onclick ='resetTransaction()'>Click To Complete the transaction</button>`
+        count1 = 0
+    }
     
 }
 function submit(){
@@ -137,7 +152,20 @@ function submit(){
         }
         count++
     }else{
-        document.getElementById("container").innerHTML =`Your Account has been blocked`
+        document.getElementById("container").innerHTML =`Your Account has been blocked for 24 hours.<button id = 'btn' onclick ='resetTransaction()'>Click To Complete the transaction</button>`
+        count = 0
     }
 
+}
+
+
+var a=0
+function display(){
+    if(a==0){
+        document.getElementById("container1").style.transform = "translate(-85%,0%)"
+        a=1
+    }else{
+        document.getElementById("container1").style.transform = "translate(-13%,0%)"
+        a=0
+    }
 }
